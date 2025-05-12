5d7120c67816f5a6968813744941e25093291f54
russ530 guevarra <guevarra.russel@iisgalvanimi.edu.it>
2025-05-12 11:58:58 +0000

    creazione della form e dei collegamenti dei componenti con l app.ts

--- Reddit-Application/src/app/app.component.ts
+++ Reddit-Application/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormComponent, PostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  return false;
+  }
 }
+//Per fare il simbolo ` premere ALT + 96 del tastierino numerico
+
